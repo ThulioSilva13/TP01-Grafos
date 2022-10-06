ec30f83eed484008ad59b4efa93b485e54d9104f
Thulio Silva <thuliosilva13@outlook.com>
2022-10-06 08:24:13 -0300

    create loop to access necessary data in json

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ from Vertice import Vertice
 with open('grafoConvertido.json', 'r') as fileJson:
     data = json.load(fileJson)
 
+listOrigin = []
+listDestiny = []
 origem = data['data']['edges']['_data']['1']['from']
 destino = data['data']['edges']['_data']['1']['to']
 peso = 0
-aresta = Aresta(origem, destino, peso)
-print("a primeira aresta começa no vetice",aresta.origem,"e vai até o vértice", aresta.destino)
+for chave, valor in data['data']['edges']['_data'].items():
+    listOrigin.append(data['data']['edges']['_data'][str(chave)]['from'])
+    listDestiny.append(data['data']['edges']['_data'][str(chave)]['to'])
+    #print("a chave é: ",chave)
+    #print("o valor é: ",valor)
+print(listOrigin)
+print(listDestiny)
+#aresta = Aresta(origem, destino, peso)
+#print("a primeira aresta começa no vetice",aresta.origem,"e vai até o vértice", aresta.destino)
